cef824b6dae2cbffa84694dd4bd779df8e01dc71
Aizada <91057556+aizadaally@users.noreply.github.com>
2022-05-18 10:44:08 +0600

    Updated age

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ name = input("Enter your name: ")
 print("your name is ", Aizada, Aizada)
 
 age = input("Enter your name: ")
-print("your name is ", 18-19)
+print("your name is ", 18)
 
 surname = input("Enter your name: ")
 print("your name is ", berdibekova)
