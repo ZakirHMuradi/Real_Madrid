9feb85cfdf28bafe96b47bb0158172e24656170f
Aizada <91057556+aizadaally@users.noreply.github.com>
2022-05-18 10:47:53 +0600

    updated cohort

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ birthday = input("Enter your name: ")
 print("your name is ",27/07/2003)
 
 cohort = input("Enter your name: ")
-print("your name is ", cohort)
+print("your name is ", cohort1 - Science cohort)
 
 name = input("Enter your name: ")
 print("your name is ", Aizada)
