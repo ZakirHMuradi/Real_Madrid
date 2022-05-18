9d5fe9ae1c2975af80d2e23db8bbda01ff60deac
ZakirHMuradi <86466557+ZakirHMuradi@users.noreply.github.com>
2022-05-18 10:20:25 +0600

    Update main.py

--- main.py
+++ main.py
@@ -34,6 +34,9 @@ print("your name is ", age)
 surname = input("Enter your name: ")
 print("your name is ", surname)
 
+birthday = input("Enter your name: ")
+print("your name is ",birthday)
+
 gender= input("Enter your name: ")
 print("your name is ", gender)
 
