9a02695323bbe7a7e796f9e0b12f5a1108e65eae
Attiullah Khan Niazi <attiullah.khanniazi_2025@ucentralasia.org>
2022-05-18 10:57:27 +0600

    condition 2

--- main.py
+++ main.py
@@ -75,3 +75,8 @@ print("4.Divide")
 while True:
     # take input from the user
     choice = input("Enter choice(1/2/3/4): ")
+    
+    # check if choice is one of the four options
+    if choice in ('1', '2', '3', '4'):
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
